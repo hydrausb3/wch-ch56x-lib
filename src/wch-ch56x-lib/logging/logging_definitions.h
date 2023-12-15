73cd8ebec334444b8ab764e1e8ffdd79c464c097
Riuzakiii <tfuchs@quarkslab.com>
2023-12-15 14:39:04 +0100

    Fix copyright notices

--- src/wch-ch56x-lib/logging/logging_definitions.h
+++ src/wch-ch56x-lib/logging/logging_definitions.h
@@ -1,5 +1,5 @@
-/********************************** (C) COPYRIGHT
-******************************* Copyright (c) 2023 Quarkslab
+/********************************** (C) COPYRIGHT *******************************
+Copyright (c) 2023 Quarkslab
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
